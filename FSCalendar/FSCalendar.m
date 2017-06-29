08d2af7153b7559b16fd2479d228c5951ec67f22
jiaxin <jiaxin@jiaxindeMacBook-Pro.local>
2017-06-29 10:12:43 +0800

    强制使用rowHeight

--- FSCalendar/FSCalendar.m
+++ FSCalendar/FSCalendar.m
@@ -992,15 +992,16 @@ typedef NS_ENUM(NSUInteger, FSCalendarOrientation) {
 - (CGFloat)preferredRowHeight
 {
     if (_preferredRowHeight == FSCalendarAutomaticDimension) {
-        CGFloat headerHeight = self.preferredHeaderHeight;
-        CGFloat weekdayHeight = self.preferredWeekdayHeight;
-        CGFloat contentHeight = self.transitionCoordinator.cachedMonthSize.height-headerHeight-weekdayHeight-_scopeHandle.fs_height;
-        CGFloat padding = 5;
-        if (!self.floatingMode) {
-            _preferredRowHeight = (contentHeight-padding*2)/6.0;
-        } else {
-            _preferredRowHeight = _rowHeight;
-        }
+//        CGFloat headerHeight = self.preferredHeaderHeight;
+//        CGFloat weekdayHeight = self.preferredWeekdayHeight;
+//        CGFloat contentHeight = self.transitionCoordinator.cachedMonthSize.height-headerHeight-weekdayHeight-_scopeHandle.fs_height;
+//        CGFloat padding = 5;
+//        if (!self.floatingMode) {
+//            _preferredRowHeight = (contentHeight-padding*2)/6.0;
+//        } else {
+//            _preferredRowHeight = _rowHeight;
+//        }
+        _preferredRowHeight = _rowHeight;
     }
     return _preferredRowHeight;
 }
